029ddd1a715f02bc5b07ccd7a39e7b2aafb45118
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-24 19:17:11 -0400

    Add Tailwind Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-<pre>{{ meals }}</pre>
+<div class="flex flex-col p-8 ">
+  <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+  <div class="flex gap-2 mt-2 justify-center">
+    <router-link to="/" v-for="letter of letters">
+      {{ letter }}
+    </router-link>
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -7,7 +14,7 @@ import { computed } from 'vue';
 import store from '../store';
 
 const meals = computed(()=>store.state.meals)
-
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 </script>
 
 <style>
